b4391b4556676bca7a4a0b13faa47b656169cca5
Valentín <valenipuente@hotmail.com>
2025-06-25 15:04:10 -0300

    Creo clase Pueblo

--- src/modelo/Pueblo.java
+++ src/modelo/Pueblo.java
@@ -1,5 +1,18 @@
 package modelo;
 
 public class Pueblo {
+    private String nombre;
 
+    public Pueblo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
