7f2dee45e8a78478cf5b552a454fca676425a95e
Valentín <valenipuente@hotmail.com>
2025-06-26 11:47:52 -0300

    Agregué comentarios a la clase Grafo

--- src/modelo/Grafo.java
+++ src/modelo/Grafo.java
@@ -6,19 +6,20 @@ import java.util.ArrayList; // Clase que implementa y soluciona los problemas de
 import java.util.List; // interfaz List
 
 public class Grafo<T> implements IGrafo<T> {
-    private List<Nodo<T>> nodos = new ArrayList<>(); // lista donde guardamos instancias de clase Nodo, que son del tipo Ubicación
-    private List<Arista<T>> aristas = new ArrayList<>(); // lista donde guardamos instancias de clase Aristas
+    private List<Nodo<T>> nodos = new ArrayList<>(); // lista donde guardamos instancias de clase Nodo, que son del tipo Ubicación!
+    private List<Arista<T>> aristas = new ArrayList<>(); // lista donde guardamos instancias de clase Aristas!
 
     @Override
     public void agregarNodo(Nodo<T> nodo) {
-        nodos.add(nodo); //
+        nodos.add(nodo); // agregamos el nodo a la lista de nodos
     }
 
     @Override
-    public void agregarArista(Nodo<T> origen, Nodo<T> destino, int peso) {
-        aristas.add(new Arista<>(origen, destino, peso));
+    public void agregarArista(Nodo<T> origen, Nodo<T> destino, int peso) { 
+        aristas.add(new Arista<>(origen, destino, peso)); // agregamos una instancia de clase arista a la lista arista
     }
-
+    
+    // getter para acceder a las listas, no setters porque no tiene sentido que alguien de afuera lo modifique!
     public List<Nodo<T>> getNodos() {
         return nodos;
     }
