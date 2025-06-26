59517d9af1df99a531369fb202dc3253f710f954
Joaco-Mar <joacomarolda@gmail.com>
2025-06-26 13:15:41 -0300

    Cambios a los pueblos Se cambiaron las subestacion por más pueblos para tenes más sentido en la vide real

--- src/modelo/Subestacion.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-public class Subestacion extends Ubicacion { // se extiende de la clase base
-	
-    public Subestacion(String nombre) {
-        super(nombre); // llama al constructor de la clase base
-    }
-
-}
--- src/test/TestGrafo.java
+++ src/test/TestGrafo.java
@@ -10,34 +10,34 @@ public class TestGrafo {
         Grafo<Ubicacion> grafo = new Grafo<>();
 
         Nodo<Ubicacion> central = new Nodo<>(new CentralElectrica("Central Eléctrica"));
-        Nodo<Ubicacion> s1 = new Nodo<>(new Subestacion("Subestación 1"));
-        Nodo<Ubicacion> s2 = new Nodo<>(new Subestacion("Subestación 2"));
-        Nodo<Ubicacion> s3 = new Nodo<>(new Subestacion("Subestación 3"));
-        Nodo<Ubicacion> p1 = new Nodo<>(new Pueblo("Pueblo A"));
-        Nodo<Ubicacion> p2 = new Nodo<>(new Pueblo("Pueblo B"));
-        Nodo<Ubicacion> p3 = new Nodo<>(new Pueblo("Pueblo C"));
+        Nodo<Ubicacion> p1 = new Nodo<>(new Pueblo("Tigre"));
+        Nodo<Ubicacion> p2 = new Nodo<>(new Pueblo("Caballito"));
+        Nodo<Ubicacion> p3 = new Nodo<>(new Pueblo("Pueblo 3"));
+        Nodo<Ubicacion> p4 = new Nodo<>(new Pueblo("Pueblo 4"));
+        Nodo<Ubicacion> p5 = new Nodo<>(new Pueblo("Pueblo 5"));
+        Nodo<Ubicacion> p6 = new Nodo<>(new Pueblo("Pueblo 6"));
 
         grafo.agregarNodo(central);
-        grafo.agregarNodo(s1);
-        grafo.agregarNodo(s2);
-        grafo.agregarNodo(s3);
         grafo.agregarNodo(p1);
         grafo.agregarNodo(p2);
         grafo.agregarNodo(p3);
+        grafo.agregarNodo(p4);
+        grafo.agregarNodo(p5);
+        grafo.agregarNodo(p6);
 
-        grafo.agregarArista(central, s1, 5);
-        grafo.agregarArista(central, s2, 7);
-        grafo.agregarArista(central, s3, 6);
-        grafo.agregarArista(s1, p1, 4);
-        grafo.agregarArista(s1, p2, 3);
-        grafo.agregarArista(s2, p2, 4);
-        grafo.agregarArista(s2, p3, 2);
-        grafo.agregarArista(s3, p3, 5);
+        grafo.agregarArista(central, p1, 5);
+        grafo.agregarArista(central, p2, 7);
+        grafo.agregarArista(central, p3, 6);
+        grafo.agregarArista(p1, p4, 4);
+        grafo.agregarArista(p1, p5, 3);
+        grafo.agregarArista(p2, p5, 4);
+        grafo.agregarArista(p2, p6, 2);
+        grafo.agregarArista(p3, p6, 5);
 
         Prim<Ubicacion> prim = new Prim<>();
         List<Arista<Ubicacion>> mst = prim.ejecutar(grafo, central);
 
-        System.out.println("Árbol de expansión mínimam con raíz desde la Central Eléctrica:");
+        System.out.println("Árbol de expansión mínima con raíz desde la Central Eléctrica:");
         for (Arista<Ubicacion> arista : mst) {
             System.out.println(arista);
         }
